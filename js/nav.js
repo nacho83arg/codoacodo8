7265b5c45f1c00ac276634a95899ef9b66bfbf0d
nacho83arg <ignacio.iannello@gmail.com>
2023-05-08 21:21:46 -0300

    cambios servicios responsive y menu

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+
+document.getElementById("menu").addEventListener("click", myfunction);
+function myfunction(){
+  document.getElementById("navegacion").classList.toggle("mostrar");
+};
+
+
+document.getElementById("lupa1").addEventListener("click", myfunction);
+function myfunction() {
+    document.getElementById("texto_s1").classList.toggle("mostrar");
+};
