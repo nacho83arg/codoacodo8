4a6132f8598f2d3800126bdc8606c58cf8c2ab40
Juan Remondegui <juaancr95@gmail.com>
2023-05-11 20:00:13 -0300

    Formulario contacto
    Validación y estilo para el formulario de contacto

---
+++ js/contacto.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#contacto').submit(function(e) {
+        var nombre = $('#form-nombre').val();
+        var mail = $('#form-mail').val();
+        var consulta = $('#form-consulta').val();
+        $('#primero').hide();
+        $('#gracias').slideDown();
+        $('#return-mail').text(mail);
+        e.preventDefault();
+    })
+})
+
+$(document).ready(function() {
+    $("#contacto").validate();
+  });
