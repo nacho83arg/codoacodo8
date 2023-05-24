d02dc1195e4a8e9092bbc7940711744d4108b484
Martin Galvan <martin.ale.galvan@gmail.com>
2023-05-24 03:34:25 -0300

    agregado lupa-menos en items de servicios

--- js/nav.js
+++ js/nav.js
@@ -5,27 +5,87 @@ function myfunction() {
 };
 
 
-document.getElementById("lupa1").addEventListener("click", myfunction2);
-function myfunction2() {
-  document.getElementById("texto_s1").classList.toggle("mostrar2");
-};
+document.getElementById("lupa1").addEventListener("click", myFunction2);
+/*function myFunction2() {
+ document.getElementById("texto_s1").classList.toggle("mostrar2");
+};*/
+function myFunction2() {
+  let image = document.getElementById("lupa1");
+  let texto = document.getElementById("texto_s1");
 
-document.getElementById("lupa2").addEventListener("click", myfunction3);
-function myfunction3() {
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa2").addEventListener("click", myFunction3);
+/*function myFunction3() {
   document.getElementById("texto_s2").classList.toggle("mostrar2");
-};
+};*/
+function myFunction3() {
+  let image = document.getElementById("lupa2");
+  let texto = document.getElementById("texto_s2");
 
-document.getElementById("lupa3").addEventListener("click", myfunction4);
-function myfunction4() {
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa3").addEventListener("click", myFunction4);
+/*function myFunction4() {
   document.getElementById("texto_s3").classList.toggle("mostrar2");
-};
+};*/
+function myFunction4() {
+  let image = document.getElementById("lupa3");
+  let texto = document.getElementById("texto_s3");
 
-document.getElementById("lupa4").addEventListener("click", myfunction5);
-function myfunction5() {
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa4").addEventListener("click", myFunction5);
+/*function myFunction5() {
   document.getElementById("texto_s4").classList.toggle("mostrar2");
-};
+};*/
+function myFunction5() {
+  let image = document.getElementById("lupa4");
+  let texto = document.getElementById("texto_s4");
 
-document.getElementById("lupa5").addEventListener("click", myfunction6);
-function myfunction6() {
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
+
+document.getElementById("lupa5").addEventListener("click", myFunction6);
+/*function myFunction6() {
   document.getElementById("texto_s5").classList.toggle("mostrar2");
-};
+};*/
+function myFunction6() {
+  let image = document.getElementById("lupa5");
+  let texto = document.getElementById("texto_s5");
+
+  if (image.getAttribute("src") === "icn/lupa.png") {
+    image.setAttribute("src", "icn/lupa-menos.png");
+    texto.classList.toggle("mostrar2");
+  } else {
+    image.setAttribute("src", "icn/lupa.png");
+    texto.classList.toggle("mostrar2");
+  }
+}
